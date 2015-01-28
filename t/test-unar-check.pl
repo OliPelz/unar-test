de54ef2f063db28ecb4ae2a94d38d9564e59b7a0
olip <olip@B110-Admins-MacBook-Air.local>
2015-01-28 21:56:48 +0100

    first commit of my work so far

---
+++ t/test-unar-check.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+
+use strict;
+
+use Test::More qw(no_plan);
+
+
+# create some test folders with subfolders
+
+# create a bunch of rar files with: password, multiarchived
+
+
+# create a crc error file
+
+# create a txt file and save as rar to prodce error
+
+# copy multi archive and miss a part
+
+
+# test against produced files
+
